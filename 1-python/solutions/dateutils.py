632be58dd7c71d49580525ab5e62cc625f204e9c
Nick Atwater <njatwate@syr.edu>
2026-02-02 15:24:08 -0500

    3:24pm

--- 1-python/solutions/dateutils.py
+++ 1-python/solutions/dateutils.py
@@ -1,1 +1,23 @@
 
+from datetime import datetime
+
+def parsedate_mdy(text: str) -> datetime:
+    """
+    Takes a string in "MM/DD/YYYY" format and returns a datetime object.
+    """
+    return datetime.strptime(text, "%m/%d/%Y")
+
+def formatdate_ymd(date: datetime) -> str:
+    """
+    Takes a datetime object and returns a formatted string in "YYYY-MM-DD" format.
+    """
+    return date.strftime("%Y-%m-%d")
+
+if __name__ == "__main__":
+    test = "12/30/2000"
+    date = parsedate_mdy(test)
+    print(date)         # datetime object
+    date_str = formatdate_ymd(date)
+    print(date_str)     # formatted string "2000-12-30"
+
+
